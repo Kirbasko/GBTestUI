8fd09335f2445901950e7a699b8df67957e365b9
kirbasko <kir.basko@gmail.com>
2022-08-04 15:35:57 +0300

    3 домашку добавил

---
+++ src/main/java/org/example/l3/StartSel.java
@@ -0,0 +1,13 @@
+package org.example.l3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StartSel {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+    }
+}
